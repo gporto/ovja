09b1bccbef09e788cf713c726ccab59597860c2e
Gonzalo Porto <gonzalo@tropo.cl>
2022-08-22 13:54:52 -0400

    en fin

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f9f04ab35e338fe7e610310cd59edaff",
-"index.html": "44e7bde5f3fef510427c3533e1643bb5",
-"/": "44e7bde5f3fef510427c3533e1643bb5",
-"main.dart.js": "ff597720076cf40d06da9e9fc1935e9a",
+"index.html": "f05c4fe8f48b3ffe41ea052556a34166",
+"/": "f05c4fe8f48b3ffe41ea052556a34166",
+"main.dart.js": "73620508726397f5a2623c26aeb6cea0",
+"flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -22,7 +23,11 @@ const RESOURCES = {
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "b37ae0f14cbc958316fac4635383b6e8",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/assets/login.json": "7bdd7c8fbd3ef0579579421bcf854154",
-"assets/assets/Logo-Amarillo.png": "490d94c63d82470369273a3ed4ebdb95"
+"assets/assets/Logo-Amarillo.png": "490d94c63d82470369273a3ed4ebdb95",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba"
 };
 
 // The application shell files that are downloaded before a service worker can
